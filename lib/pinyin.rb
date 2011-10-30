7bdef1d02d967b94ff8bf83a4ce10422fdfa5315
tom2cjp <tom.cjp.cn@gmail.com>
2011-10-30 15:28:25 +0800

    only zh need find etymon

--- lib/pinyin.rb
+++ lib/pinyin.rb
@@ -4,13 +4,11 @@ class Pinyin
   @@dist = YAML.load_file(File.dirname(__FILE__) + "/../dist.yml")
 
   def self.full(value)
-    ori = value
-    words = ori.split(//)
-    words.each do |w|
-      etymon = find_etymon(w)
-      ori.sub!(w, etymon) if etymon
-    end
-    ori
+    words = value.to_s.split(//)
+    words.map do |w|
+      etymon = find_etymon(w) if zh_cn?(w)
+      etymon || w
+    end.join
   end
 
   def self.find_etymon(word)
@@ -23,4 +21,10 @@ class Pinyin
 
     etymon
   end
+
+  private
+
+    def zh_cn?(w)
+      w.length != 1
+    end
 end
